c6acd901b1d343e5ffcbc915f5f15a7062db43c9
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-06-28 21:35:29 +0530

    feat: remove calls to pokeapi

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,6 @@
 export async function getPokemonCardInfo(id) {
-  const pokeApiRes = await fetch("https://pokeapi.co/api/v2/pokemon/" + id);
-  const pokeJson = await pokeApiRes.json();
-  console.log(pokeJson.name);
-
   return {
-    id: pokeJson.id,
+    id,
     imgUrl:
       "https://picsum.photos/seed/" + Math.random().toString() + "/250.webp",
   };
